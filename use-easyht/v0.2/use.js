87a234c953bc70a7bca16e563bffa5942195c464
YellowFace <68597070+yellowface233@users.noreply.github.com>
2020-07-23 18:56:56 +0800

    Create use.js

---
+++ use-easyht/v0.2/use.js
@@ -0,0 +1,18 @@
+//EasyHT JavaScript
+//Version.2.0
+//官网：https://easyht.js.org/
+
+//控制台日志_加载中与信息、作者
+console.log('EasyHT-JS 正在加载中')
+console.log('本页部分美化效果来自EasyHT！')
+console.log('EasyHT https://easyht.js.org/')
+console.log('Powered by YellowFace 2018')
+console.log('YellowFace's GitHub:https://github.con/yellowface233')
+
+//代码框收缩 
+function easyht0001(){
+document.getElementById('easyhtcode').style.display='none'
+}
+function easyht0002(){
+document.getElementById('easyhtcode').style.display='block'
+}
